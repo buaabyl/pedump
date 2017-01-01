8a3b8777c183861a6f80b57458896971eaf9e716
buaa.byl <buaa.byl@gmail.com>
2017-01-01 20:21:41 +0800

    minor fix

--- pygetopt.c
+++ pygetopt.c
@@ -13,13 +13,14 @@
  *  along with this program; see the file COPYING.  If not, write to
  *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
  *
+ *  2016.07.25  first version
+ *              1. support short format
+ *
  *  2016.09.02  buaabyl
  *              1. reconstructed control flow
  *              2. support long format
  *              3. python getopt format
  *
- *  2016.07.25  first version
- *              1. support short format
  *
  */
 #include <stdio.h>
